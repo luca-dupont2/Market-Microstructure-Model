f59698d2efd2f4c407c2a74c2a52bee3076ff995
luca-dupont2 <2430291@brebeuf.qc.ca>
2025-09-25 09:23:43 -0400

    Change init class names

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,12 +1,13 @@
 from .base_strategy import BaseStrategy
-from .market_maker import MarketMaker
+from .market_maker import SymmetricMaker, InventoryMaker
 from .taker import ManualTaker, MomentumTaker, ImbalanceTaker
 from .execution import ExecutionStrategy, TWAPExecution, BlockExecution
 from .signal import BaseSignal, MomentumSignal, ImbalanceSignal
 
 __all__ = [
     "BaseStrategy",
-    "MarketMaker",
+    "SymmetricMaker",
+    "InventoryMaker",
     "ManualTaker",
     "TWAPExecution",
     "ExecutionStrategy",
